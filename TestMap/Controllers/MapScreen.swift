4b84350635a0a7a23c02bd6dc8c8e967655c0b0f
Aaron Shackelford <burntcreamlatte@icloud.com>
2020-01-12 13:18:55 -0700

    add reverse geolocation

--- TestMap/Controllers/MapScreen.swift
+++ TestMap/Controllers/MapScreen.swift
@@ -13,9 +13,11 @@ import CoreLocation
 class MapScreen: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addressLabel: UILabel!
     
     let locationManager = CLLocationManager()
     let regionInMeters: Double = 10000
+    var previousLocation: CLLocation?
     
     
     override func viewDidLoad() {
@@ -51,10 +53,7 @@ class MapScreen: UIViewController {
         case .authorizedAlways:
             break
         case .authorizedWhenInUse:
-            //would test to determine when phone suspended
-            centerViewOnUserLocation()
-            //self.mapView.showsUserLocation = true
-            locationManager.startUpdatingLocation()
+            startTrackingUserLocation()
         case .notDetermined:
             locationManager.requestWhenInUseAuthorization()
         case .restricted:
@@ -65,18 +64,69 @@ class MapScreen: UIViewController {
             break
         @unknown default:
             fatalError()
+            print("Unknown error occured during checking location authorization permissions.")
         }
+    }
+    
+    func startTrackingUserLocation() {
+        //would test to determine when phone suspended
+        //refocuses view onto center of user; will need to allow people to look around map
+        centerViewOnUserLocation()
+        //self.mapView.showsUserLocation = true
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
     }
 }
 
-extension MapScreen: MKMapViewDelegate, CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-        mapView.setRegion(region, animated: true)
-    }
+extension MapScreen: CLLocationManagerDelegate {
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.last else { return }
+//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+//        mapView.setRegion(region, animated: true)
+//    }
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkLocationAuthorization()
+    }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+}
+
+extension MapScreen: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        
+        guard let previousLocation = self.previousLocation else { return }
+        //must me more than 50 meters difference before updates request for location
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation = center
+        
+//        After initiating a reverse-geocoding request, do not attempt to initiate another reverse- or forward-geocoding request. Geocoding requests are rate-limited for each app, so making too many requests in a short period of time may cause some of the requests to fail. When the maximum rate is exceeded, the geocoder passes an error object with the value CLError.Code.network to your completion handler.
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+            guard let self = self else { return }
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let placemark = placemarks?.first else {
+                //show alert to user
+                return
+            }
+            
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streetName = placemark.thoroughfare ?? ""
+            
+            DispatchQueue.main.async {
+                self.addressLabel.text = "\(streetNumber) \(streetName)"
+            }
+        }
     }
 }
